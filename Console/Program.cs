7f9234ef42d40fffaa1b2977380bb4444db87c51
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-07-05 19:17:03 +0300

    feat: The project was created and the necessary layers for the car were created

---
+++ Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryProductDal());
+            
+            carManager.Add(new Car { CarId = 4, BrandId = 3, ColorId = 3, ModelYear = "2015", DailyPrice = 30000, Description = "Porsche" });
+            
+            var result = carManager.GetAll();
+            
+            carManager.Delete(result[0]);
+            
+            result[1].DailyPrice = 18000;
+            carManager.Update(result[1]);
+            
+            foreach (var car in result)
+            {
+                System.Console.WriteLine(car.Description + " " + car.DailyPrice);
+            }
+
+            System.Console.WriteLine("----");
+            System.Console.WriteLine(carManager.GetById(4).Description);
+
+            
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryProductDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,ModelYear="1990",DailyPrice=10000,Description="BMW"},
+                new Car{CarId=2,BrandId=1,ColorId=2,ModelYear="2000",DailyPrice=15000,Description="Audi"},
+                new Car{CarId=3,BrandId=2,ColorId=1,ModelYear="2010",DailyPrice=20000,Description="Bugatti"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.FirstOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.SingleOrDefault(c => c.CarId == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.CarId = car.CarId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
