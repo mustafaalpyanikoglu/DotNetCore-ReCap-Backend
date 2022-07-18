018c220cd85280c5a89ebd7242d690e83110b53f
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-07-18 21:19:48 +0300

    feat: customer user and car rental system integrated

---
+++ Business/Abstract/IRentalService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Utilities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IRentalService
+    {
+        IDataResult<List<Rental>> GetAll();
+        IResult Add(Rental rental);
+        IResult Delete(Rental rental);
+        IResult Update(Rental rental);
+    }
+}
---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Abstract;
+using Core.Utilities.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        private IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental rental)
+        {
+            var rentalToAdd = _rentalDal.Get(r => r.CarId == rental.CarId);
+            if((rental.RentDate == null || rental.ReturnDate > rental.RentDate))
+            {
+                _rentalDal.Add(rental);
+                return new SuccessResult(Messages.CarHired);
+            }
+            else
+            {
+                return new ErrorResult(Messages.CarNotDelivered);
+            }
+            
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult(Messages.Deleted);
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll(), Messages.ProductsListed);
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+            return new SuccessResult(Messages.Updated);
+        }
+    }
+}
